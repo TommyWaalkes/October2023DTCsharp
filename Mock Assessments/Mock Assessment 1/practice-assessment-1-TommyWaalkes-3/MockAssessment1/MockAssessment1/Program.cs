3a5fb45a1af42977a67bc9f48eae969468083305
TommyWaalkes <tommy@grandcircus.co>
2023-10-04 11:40:36 -0400

    Added in method demos

---
+++ Mock Assessments/Mock Assessment 1/practice-assessment-1-TommyWaalkes-3/MockAssessment1/MockAssessment1/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace MockAssessment1
+{
+    public class Program
+    {
+
+        //Assessments won't ask you to write code in your main method, you will not be graded on whats there 
+        //That being you are encouraged to use main to test your code
+        public static void Main() {
+            bool same = AreTheSame(10, 11);
+            Console.WriteLine(same);
+            bool same1 = AreTheSame(10, 10);
+            Console.WriteLine(same1);
+
+            double result = Subtract(15, 10);
+            Console.WriteLine(result);
+            double result2 = Subtract(12, 15);
+            Console.WriteLine(result2);
+
+            Console.WriteLine(FindBuildingType(2));
+            Console.WriteLine(FindBuildingType(6));
+            Console.WriteLine(FindBuildingType(15));
+            Console.WriteLine(FindBuildingType(200));
+        }
+
+        //Parameters are like loop variables, they exist purely within the method for the methods own use
+        public static bool AreTheSame(int s1, int s2)
+        {
+            //Return statements pass data up to where the method is called 
+            //They must match the return setup in the method signature
+            return s1 == s2;
+        }
+
+        public static double Subtract(double num1, double num2)
+        {
+            double output = num1 - num2;
+            return output;
+        }
+
+        public static string FindBuildingType(int floors)
+        {
+            if(floors < 0)
+            {
+                return "Negative floors";
+            }
+            else if(floors <=3)
+            {
+                return "This is a house";
+            }
+            else if (floors <= 10)
+            {
+                return "This is an office building";
+            }
+            else if (floors <= 49)
+            {
+                return "This is a sky scraper";
+            }
+            else
+            {
+                return "This is a SUPER skyscraper";
+            }
+        }
+    }
+}
---
+++ Week 1 - Fundamentals/MethodsDemo/MethodsDemo/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace MethodsDemo
+{
+    internal class Program
+    {
+        //Main is a method, but it is a special one 
+        //When you run your code C# looks and starts in main 
+        //Main as a method name is reserved for that use
+        static void Main(string[] args)
+        {
+            bool goOn = true;
+            while (goOn)
+            {
+                Console.WriteLine("Hello, World!");
+                double a = GetAreaRectangle(10, 15);
+                Console.WriteLine(a);
+
+                PrintGreeting("Tommy");
+
+                goOn = AskToContinue();
+            }
+        }
+
+        //Main can't look at the contents of a method, just its result IE what it returns
+        public static double GetAreaRectangle(double length, double width)
+        {
+            //The method is done once you hit the return statement 
+            double area = length * width;
+            return area;
+        }
+
+        public static void PrintGreeting(string name)
+        {
+            string greeting = $"Hello, {name}. I hope you are doing well.";
+            Console.WriteLine(greeting);
+
+            //Say you're in a void method and want it to stop 
+            return; //<-- this kicks you of the method, I believe only C# and Javascript will allow you to do that
+        }
+
+        public static bool AskToContinue()
+        {
+            Console.WriteLine("Would you like to run again? y/n");
+            string input = Console.ReadLine();
+
+            //3 case we care about: y, n, or anything else
+            if(input == "y")
+            {
+                return true;
+            }
+            else if(input == "n")
+            {
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("Hey I didn't understand your response");
+                return AskToContinue();
+            }
+        }
+
+    }
+}
