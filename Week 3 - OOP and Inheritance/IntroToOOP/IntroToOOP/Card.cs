0547a71d5727a809f1db8c5ab4aaa6bfb24f0f38
TommyWaalkes <tommy@grandcircus.co>
2023-10-16 14:00:48 -0400

    Added War intro to objects example

---
+++ Week 3 - OOP and Inheritance/IntroToOOP/IntroToOOP/Card.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroToOOP
+{
+    //Card is a model - Models are meant to contain and represent data 
+    internal class Card
+    {
+        //1) Properties - Suit, Value: number values, Jack, Queen, King, Joker (get set) 
+        //2) Constructors - fill in values via parameters, no other setup is needed
+        //3) Methods - Random Generate card and shuffling
+        //You should ask yourself, which parts will I need, and what do those want to have in them. 
+
+        public string Suit { get; set; } 
+        public int Value { get; set; } 
+
+        public Card(string suit, int value) {
+            Suit = suit; 
+            Value = value;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine("Value: " +Value +" of "+  Suit);
+        }
+
+        public static Card GetRandomCard()
+        {
+            Random r = new Random();
+            int value = r.Next(1, 15);
+
+            string[] suits = { "Spades", "Hearts", "Diamonds", "Clubs" };
+            int suitPick = r.Next(0, suits.Length);
+            string suit = suits[suitPick];
+
+            //New keyword means we're calling a constructor 
+            Card output = new Card(suit, value);
+            return output;
+        }
+    }
+}
---
+++ Week 3 - OOP and Inheritance/IntroToOOP/IntroToOOP/WarGame.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroToOOP
+{
+    //This is a runner class, its job is to delegate to other classes decide who needs to run and when 
+    //Most functionality lives in runner classes 
+    internal class WarGame
+    {
+        //1) Properties - wins and losses, players (actually lets make a plsyer class), deck lists 
+        //2) Constructors - Player1 and Player2 objects, fill out decks 
+        //3) Methods - game logic - compare the draws between the 2 players
+        List<Card> p1Deck { get; set; } = new List<Card>();
+        List<Card> p2Deck { get; set; } = new List<Card>();
+        int currentDeckIndex = 0; 
+        List<Card> p1Discard { get; set; } = new List<Card>();
+        List<Card> p2Discard { get; set; } = new List<Card>();
+
+        public WarGame() { 
+            for(int i = 0; i < 10; i++)
+            {
+                Card p1Card = Card.GetRandomCard();
+                Card p2Card = Card.GetRandomCard();
+                p1Deck.Add(p1Card);
+                p2Deck.Add(p2Card);
+            }
+            Shuffle(p2Deck);
+            Shuffle(p1Deck);
+
+        }
+
+        public void Shuffle(List<Card> deck)
+        {
+            Random r = new Random();
+            int n = deck.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = r.Next(n + 1);
+                Card value = deck[k];
+                deck[k] = deck[n];
+                deck[n] = value;
+            }
+        }
+
+        public void RunTurn()
+        {
+            Card p1 = p1Deck[currentDeckIndex];
+            Card p2 = p2Deck[currentDeckIndex];
+
+            if(p1.Value > p2.Value)
+            {
+                //Player 1 wins the round 
+                p1Deck.Remove(p1);
+                p2Deck.Remove(p2);
+                p1Discard.Add(p1);
+                p1Discard.Add(p2);
+            }
+        }
+
+    }
+}
