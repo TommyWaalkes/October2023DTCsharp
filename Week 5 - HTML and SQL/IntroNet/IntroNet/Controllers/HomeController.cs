63810b20ae9715aecc83cfbfeb48b0f8e70a54b4
TommyWaalkes <tommy@grandcircus.co>
2023-11-01 16:09:21 -0400

    Added Intro to .Net

---
+++ Week 5 - HTML and SQL/IntroNet/IntroNet/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using IntroNet.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace IntroNet.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            //Return view grabs and displays the matching cshtml file
+            return View();
+        }
+
+        public IActionResult AnimalForm()
+        {
+            Animal a = new Animal();
+            return View(a);
+        }
+
+        //This is some .Net magic, .Net is able to see the form input we just did 
+        //And automatically map it to the property in my action. 
+        public IActionResult Animal(Animal a)
+        {
+            //Animal a = new Animal() {
+            //    Id = 1,
+            //    Name = "Herbert",
+            //    CaloriesNeeded = 4000,
+            //    Species = "Elephant",
+            //    HasClaws = false
+            //};
+
+            //This makes the model availiable in the view. 
+            return View(a);
+        }
+    }
+}
---
+++ Week 5 - HTML and SQL/IntroNet/IntroNet/Models/Animal.cs
@@ -0,0 +1,14 @@
+﻿namespace IntroNet.Models
+{
+    public class Animal
+    {
+        //Models are generally just a series of properties. Maybe sometimes a constructor and sometimes code to handle conversions. 
+        //All properties must be public and most often will just use get and set unmodified. 
+        public int Id {  get; set; }
+        public string Name { get; set; }
+        public string Species { get; set; }
+        public int CaloriesNeeded {  get; set; }
+        public bool HasClaws { get; set; }
+
+    }
+}
