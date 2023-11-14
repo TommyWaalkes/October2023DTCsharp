95e916fd10fd36ec18e1eb491af7720fd09e21c4
TommyWaalkes <tommy@grandcircus.co>
2023-11-14 13:52:58 -0500

    Added Angular intro

---
+++ Week 7 - Typescript and Angular/Intro to Angular/Intro/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  //This is how you call this component elsewhere. We will get into 
+  //selectors more when we do multiple components.
+  selector: 'app-root',
+
+  //This is the url for the view file. Generally the convention is to name 
+  //the view and controller the same thing. 
+  templateUrl: './app.component.html',
+  //This points to the stylesheet that exist just for this view. 
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Intro';
+  //Any variable in the component class are accesible in the html view 
+  names:string[] =["Jimmy", "Timmy", "Kimmy"];
+}
