b057e0a54990629ceae26a73ed6790d420c73af8
TommyWaalkes <tommy@grandcircus.co>
2023-11-14 14:07:25 -0500

    Added Intro to Angular

--- Week 7 - Typescript and Angular/Intro to Angular/Intro/src/app/app.component.ts
+++ Week 7 - Typescript and Angular/Intro to Angular/Intro/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Student } from './student';
 
 @Component({
   //This is how you call this component elsewhere. We will get into 
@@ -15,4 +16,5 @@ export class AppComponent {
   title = 'Intro';
   //Any variable in the component class are accesible in the html view 
   names:string[] =["Jimmy", "Timmy", "Kimmy"];
+  stu:Student = {name:"Jake", favoriteFood:"Hamburgers", hometown:"Detroit"}
 }
---
+++ Week 7 - Typescript and Angular/Intro to Angular/Intro/src/app/student.ts
@@ -0,0 +1,5 @@
+export interface Student {
+    name:string; 
+    hometown:string; 
+    favoriteFood:string; 
+}
