27a6cf7c30098ba832072588e8950eafcad536ee
TommyWaalkes <tommy@grandcircus.co>
2023-11-21 09:06:20 -0500

    Added Shape demo

--- Week 8 - Services and Full Stack/Services/src/app/app.component.ts
+++ Week 8 - Services and Full Stack/Services/src/app/app.component.ts
@@ -9,23 +9,41 @@ import { AreaService } from './area.service';
 export class AppComponent {
   title = 'Services';
   len:number = 10;
+  wid:number = 1; 
   area:number = 0; 
-  style:string = `width: ${this.len}; height: ${this.len}; background-color: red;`;
+  shapes:string[] = [
+    "square",
+    "triangle",
+    "rectangle"
+  ];
+
+  selected:string = this.shapes[0];
+
+  getSquareArea(){
+    this.area = this.len * this.len;
+  }
+  getRectangleArea(){
+    this.area = this.len * this.wid;
+  }
+
+  getTriangleArea(){
+    this.area = this.len * this.wid / 2;
+  }
+
+  getTriangle(){
+    return {
+      height:0, 
+      width: 0, 
+      borderLeft: `${this.len * 5}px solid transparent `, 
+      borderRight:`${this.len * 5}px solid transparent `,
+      borderBottom:`${this.wid * 5}px solid red`   }
+  }
+
+
   //This pops a service object into existance for the rest of this component to use 
   //This is where the depency injection is happening in Angular
   constructor(private areaService:AreaService){
 
   }
 
-  getSquareArea(){
-    this.area = this.areaService.getSquare(this.len);
-    //this.style = `width: '${this.len}'; height: '${this.len}'; background-color: red;`;
-    document.getElementById("Square")?.style.width = this.len;
-    document.getElementById("Square")?.style.height = this.len;
-
-  }
-
-  getLen(){
-    return this.len;
-  }
 }
