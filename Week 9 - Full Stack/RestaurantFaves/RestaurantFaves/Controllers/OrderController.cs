d42ee918d410268ec8f660a4ed149e8ec077e9e7
TommyWaalkes <tommy@grandcircus.co>
2023-11-27 09:52:42 -0500

    Added Faves Backend

---
+++ Week 9 - Full Stack/RestaurantFaves/RestaurantFaves/Controllers/OrderController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestaurantFaves.Models;
+
+namespace RestaurantFaves.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OrdersController : ControllerBase
+    {
+        RestaurantFavesContext db = new RestaurantFavesContext();
+
+        [HttpGet]
+        public List<Order> GetOrders()
+        {
+            return db.Orders.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public Order GetOrder(int id)
+        {
+            Order output = db.Orders.Find(id);
+            return output;
+        }
+
+        [HttpPost]
+        public void AddOrder(Order order)
+        {
+            db.Orders.Add(order);
+            db.SaveChanges();
+        }
+
+        [HttpPost("{id}")]
+        public void UpdateOrder(int id, Order body)
+        {
+            body.Id = id;
+            db.Orders.Update(body);
+
+            db.SaveChanges();
+        }
+
+        [HttpDelete("{id}")]
+        public void DeleteOrder(int id)
+        {
+            Order deleted = db.Orders.Find(id);
+            db.Orders.Remove(deleted);
+            db.SaveChanges();
+        }
+    }
+}
---
+++ Week 9 - Full Stack/RestaurantFaves/RestaurantFaves/Models/RestaurantFavesContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace RestaurantFaves.Models;
+
+public partial class RestaurantFavesContext : DbContext
+{
+    public RestaurantFavesContext()
+    {
+    }
+
+    public RestaurantFavesContext(DbContextOptions<RestaurantFavesContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Order> Orders { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=.\\SQLExpress;Database=RestaurantFaves;Trusted_Connection=True;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Order>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Order__3213E83F4E75C17D");
+
+            entity.ToTable("Order");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Description)
+                .HasMaxLength(50)
+                .HasColumnName("description");
+            entity.Property(e => e.OrderAgain).HasColumnName("orderAgain");
+            entity.Property(e => e.Rating).HasColumnName("rating");
+            entity.Property(e => e.Restaurant)
+                .HasMaxLength(40)
+                .HasColumnName("restaurant");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
