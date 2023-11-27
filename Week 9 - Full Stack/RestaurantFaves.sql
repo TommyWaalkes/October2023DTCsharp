3db58b49d3ca69787af36d3201a0761f575a3702
TommyWaalkes <tommy@grandcircus.co>
2023-11-27 09:53:35 -0500

    Added SQL File

---
+++ Week 9 - Full Stack/RestaurantFaves.sql
@@ -0,0 +1,16 @@
+--create database RestaurantFaves; 
+
+--create table [Order] (
+--	id int primary key identity(1,1), 
+--	[description] nvarchar(50),
+--	restaurant nvarchar(40), 
+--	rating int, 
+--	orderAgain bit
+--)
+
+--insert into [Order] 
+--values( 'Good Breakfast food', 'Waffle House', 4, 1), 
+--	  ('Fine dining and drink', 'Mac Fancies', 3, 0),
+--	  ('Hipster Gastro pub, with toast', 'FruYus', 5, 1), 
+--	  ('Cheap and tasty Pizza', 'Marios Pizzaria', 5, 1), 
+--	  ('Quality seafood cooked in Garlic','Jimmys Cuss Free Seafood', 1, 0)
